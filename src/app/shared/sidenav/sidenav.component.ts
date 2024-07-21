705e83b5e8b8b2c7bce9e2c70b2afef4de02f93f
Tom Bragança <gleydiston35@gmail.com>
2024-07-21 15:09:40 -0300

    chore: Add sidenav component and update app layout

---
+++ src/app/shared/sidenav/sidenav.component.ts
@@ -0,0 +1,58 @@
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HeaderComponent } from '../header/header.component';
+
+/** @title Responsive sidenav */
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: 'sidenav.component.html',
+  styleUrl: 'sidenav.component.scss',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    HeaderComponent,
+  ],
+})
+export class SidenavComponent implements OnDestroy {
+  mobileQuery: MediaQueryList;
+
+  sideNavOptions = [
+    { name: 'Meus baralhos', icon: 'deck', route: '/decks' },
+    { name: 'Pokédex', icon: 'pokedex', route: '/pokedex' },
+  ]
+
+  fillerContent = Array.from(
+    { length: 50 },
+    () =>
+      `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+       laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+       voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+       cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+  );
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+}
+
+/**  Copyright 2024 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
